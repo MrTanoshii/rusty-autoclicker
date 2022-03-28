9c725e079929925c123c508de127c072555025f9
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-03-28 18:18:41 +0400

    feat: Add eframe_template and rust deps

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] //Hide console window in release builds on Windows, this blocks stdout.
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    let app = rusty_autoclicker::TemplateApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
+}
