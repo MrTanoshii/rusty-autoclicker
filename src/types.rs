046200114b0bcfe020715e4726e1105ce7131b3b
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-11-24 02:20:06 +0400

    clippy: derive `Eq`

--- src/types.rs
+++ src/types.rs
@@ -1,16 +1,16 @@
-#[derive(PartialEq, Debug, Copy, Clone)]
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 pub enum ClickType {
     Single,
     Double,
 }
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 pub enum ClickPosition {
     Mouse,
     Coord,
 }
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 pub enum AppMode {
     Bot,
     Humanlike,
