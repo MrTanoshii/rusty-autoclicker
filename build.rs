311ba14c38b464bb2316795a00bf51ce04380b65
Maksim Bondarenkov <119937608+ognevny@users.noreply.github.com>
2026-03-23 10:02:28 +0300

    build: fix build for native MinGW build

--- build.rs
+++ build.rs
@@ -5,12 +5,14 @@ fn main() -> io::Result<()> {
     if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
         let mut res = WindowsResource::new();
         let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
+        let host = std::env::var("HOST").unwrap();
         match env.as_str() {
             "gnu" => {
-                // Not sure whether this works the same if build on windows; needs testing
-                // Perfectly fine if cross compiling from linux
-                res.set_ar_path("x86_64-w64-mingw32-ar")
-                    .set_windres_path("x86_64-w64-mingw32-windres");
+                // Set proper binutils for cross-compilation
+                if !host.contains("windows-gnu") {
+                    res.set_ar_path("x86_64-w64-mingw32-ar")
+                        .set_windres_path("x86_64-w64-mingw32-windres");
+                }
             }
             "msvc" => {}
             _ => panic!("unsupported target-env: {env}"),
