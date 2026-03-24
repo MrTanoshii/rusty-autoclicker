a3da69989a95cc2c1428e0e7a2687e13747404da
T. C. Vincent Leung Yin Ko <47116127+MrTanoshii@users.noreply.github.com>
2026-03-24 02:08:11 -0400

    build: update as per winresource docs

--- build.rs
+++ build.rs
@@ -1,24 +1,9 @@
-use std::io;
 use winresource::WindowsResource;
 
-fn main() -> io::Result<()> {
-    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = WindowsResource::new();
-        let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-        let host = std::env::var("HOST").unwrap();
-        match env.as_str() {
-            "gnu" => {
-                // Set proper binutils for cross-compilation
-                if !host.contains("windows-gnu") {
-                    res.set_ar_path("x86_64-w64-mingw32-ar")
-                        .set_windres_path("x86_64-w64-mingw32-windres");
-                }
-            }
-            "msvc" => {}
-            _ => panic!("unsupported target-env: {env}"),
-        };
-        res.set_icon("assets/icon-256.ico");
-        res.compile()?;
+        res.set_icon("assets/icon-256.ico").set_language(0x0409); // English (US)
+        res.compile().unwrap();
     }
-    Ok(())
 }
